5e7a16e43d7bc99feaae58c36e2c86489217de6e
unknown <nayakaputra773@gmail.com>
2024-04-28 13:16:55 +0700

    first commit

---
+++ resources/views/testailwind.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-slate-200">
+    <h1 class="text-blue-200 font-bold">Woi Ini dah bisa lo</h1>
+</body>
+</html>
