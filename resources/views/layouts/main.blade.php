b86a6a25d4efed0f7ef4fc8638a903e782952325
FildzaR <fildzarasyika@students.usu.ac.id>
2024-05-08 08:30:58 +0700

    melengkapi halaman login

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,6 @@
     @yield('container')
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
-    <script src="js/script.js"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
