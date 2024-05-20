87329aa15a7e5b36f490ef57dc5d2f4da45c1ac9
unknown <nayakaputra773@gmail.com>
2024-05-20 13:53:52 +0700

    backend neworder.

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -55,7 +55,7 @@
                 @foreach($menu as $menus)
                 @if($menus->id_ktgmenu == $kategoris->id_ktgmenu)
                 <div class="rounded-md shadow-lg overflow-hidden mb-7 bg-transparent w-1/3 md:w-80">
-                    <img src="data:image/jpeg;base64,{{$menus->gambar_menu}}" alt="Image Caption" class="w-full">
+                    <img src="{{ asset('img/menu/' . $menus->gambar_menu) }}" alt="Image Caption" class="w-full h-48 object-cover">
                     <div class="px-2 py-2">
                       <div class="font-bold text-lg mb-1">{{$menus->nama_menu}}</div>
                       <p class="text-xs mb-1 text-gray-600">{{$menus->keterangan}}</p>
