1cfdf101f8476c7aeb96ff450d0d272a26aa9a79
unknown <nayakaputra773@gmail.com>
2024-06-10 10:38:51 +0700

    kitchen

--- resources/views/kitchen/detail.blade.php
+++ resources/views/kitchen/detail.blade.php
@@ -26,7 +26,7 @@
 
             <!-- pesanan 1 start -->
             @foreach($details as $detail)
-            @if($detail->progress === 'Dimasak')
+            @if($detail->progress === 'Dimasak' && $detail->menu->ktgmenu->jenis === 'Makanan')
             <div class="w-full">
                 <!-- detail pesanan start -->
                 <div class="gap-3">
@@ -59,7 +59,7 @@
 
                 <!-- button perbarui start -->
                 <div data-modal-target="default-modal" data-modal-toggle="default-modal" class="items-center mt-3 w-full">
-                    <button data-detail-id="{{ $detail->menu->nama_menu }}" data-order-id="{{ $detail->note }}" data-order-jumlah="{{ $detail->jumlah }}" gambar="{{ $detail->menu->gambar_menu }}"  type="button" class="openModalBtn w-full items-center justify-center text-white bg-[#FFD369] hover:bg-[#edca69] font-medium rounded-lg text-sm p-2 text-center">Perbarui</button>
+                    <button data-tail-id="{{ $detail->id_order_details }}" data-der-id="{{ $detail->id_order }}" data-detail-id="{{ $detail->menu->nama_menu }}" data-order-id="{{ $detail->note }}" data-order-jumlah="{{ $detail->jumlah }}" gambar="{{ $detail->menu->gambar_menu }}"  type="button" class="openModalBtn w-full items-center justify-center text-white bg-[#FFD369] hover:bg-[#edca69] font-medium rounded-lg text-sm p-2 text-center">Perbarui</button>
                 </div>
                 <!-- button perbarui end -->
             </div>
@@ -82,7 +82,7 @@
 
             <!-- pesanan 1 start -->
             @foreach($details as $detail)
-            @if($detail->progress === 'Siap Disajikan')
+            @if($detail->progress === 'Siap Disajikan' && $detail->menu->ktgmenu->jenis === 'Makanan' )
             <div class="w-full">
                 <!-- detail pesanan start -->
                 <div class="gap-3">
@@ -118,36 +118,7 @@
             <!-- pesanan 1 end -->
 
             <!-- pesanan 2 start -->
-            <div class="w-full">
-                <!-- detail pesanan start -->
-                <div class="gap-3">
-                    <!-- Detail Pesanan Start -->
-                    <div class="flex items-start flex-row justify-start gap-3">
-                            <!-- gambarnya -->
-                            <div>
-                                <img class="w-10" src="https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
-                            </div>
 
-                            <!-- menu notes jumlah -->
-                            <div>
-                                <h3 class="font-bold">
-                                    Menu 1
-                                </h3>
-                                <p class="text-slate-400">
-                                    Notes:
-                                </p>
-                                
-                                <div class="bg-[#FFD369] rounded-xl flex p-1 items-center justify-center">
-                                    <p>1</p>
-                                </div>
-                            </div>
-                            <!-- menu notes jumlah end -->
-                    </div>
-                    <!-- Detail Pesanan End -->
-
-                </div>
-                <!-- detail pesanan stop -->
-            </div>
             <!-- pesanan 2 end -->
         </div>
 
@@ -166,6 +137,9 @@
                     </button>
                 </div>
                 <!-- Modal body -->
+                <form id="updateForm" action="{{ route('kitchen.update', ['id_order' => ':id_order', 'id_order_details' => ':id_order_details']) }}" method="POST">
+                    @csrf
+                    @method('PUT')
                 <div class="space-y-4">
                     <!-- Modal content here -->
                     <div class="flex items-start p-4 flex-row gap-3">
@@ -180,19 +154,20 @@
                         </div>
                         <!-- menu notes jumlah end -->
                     </div>
-        
+                    <input type="hidden" id="status" name="progress" value="Siap Disajikan">
                     <div class="flex justify-center mt-6">
-                        <button id="saveModalBtn" class="bg-yellow-400 text-white px-4 py-2 rounded-3xl mr-2" data-modal-save="default-modal">Perbarui</button>
-                        <button id="cancelModalBtn" class="bg-red-500 text-white px-4 py-2 rounded-3xl" data-modal-cancel="default-modal">Batal</button>
+                        <button type="submit" id="saveModalBtn"  class="bg-yellow-400 text-white px-4 py-2 rounded-3xl mr-2" data-modal-save="default-modal">Perbarui</button>
+                        <button type="button" id="cancelModalBtn" class="bg-red-500 text-white px-4 py-2 rounded-3xl" data-modal-cancel="default-modal">Batal</button>
                     </div>
                 </div>
+                </form>
         
             </div>
         </div>
         <!-- MODAL END -->
 
         <!-- Modal Konfirmasi Start -->
-        <div id="confirm-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex items-center justify-center modal">
+        {{-- <div id="confirm-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed inset-0 z-50 flex items-center justify-center modal">
             <div class="fixed inset-0 bg-black bg-opacity-50"></div>
             <div class="relative bg-white rounded-lg shadow-lg p-6 w-1/3">
                 <!-- Modal header -->
@@ -206,42 +181,52 @@
                     </button>
                 </div>
                 <!-- Modal body -->
+                
                 <div class="space-y-4" id="menuModal">
                     <p>Apakah Anda yakin ingin menyimpan perubahan?</p>
                     <div class="flex justify-center mt-6">
-                        <button id="confirmSaveBtn" class="bg-yellow-400 text-white px-4 py-2 rounded-3xl mr-2" data-modal-confirm-save="confirm-modal">Yes</button>
-                        <button id="cancelConfirmBtn" class="bg-red-500 text-white px-4 py-2 rounded-3xl" data-modal-confirm-cancel="confirm-modal">No</button>
+                        <button type="submit" data-detail-id="{{$detail->id_detail}}" data-order-id="{{$detail->id_order}}"  id="confirmSaveBtn" class="submit-button bg-yellow-400 text-white px-4 py-2 rounded-3xl mr-2" data-modal-confirm-save="confirm-modal">Yes</button>
+                    
+                        <button  id="cancelConfirmBtn" class="bg-red-500 text-white px-4 py-2 rounded-3xl" data-modal-confirm-cancel="confirm-modal">No</button>
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
         <!-- Modal Konfirmasi End -->
 
 
     <script>
         document.addEventListener('DOMContentLoaded', function() {
         const openModalButtons = document.querySelectorAll('.openModalBtn');
+        const updateButtons = document.querySelectorAll('.submit-button');
         const modal = document.getElementById('menuModal');
         const modalTitle = document.getElementById('tittle');
         const modalNotes = document.getElementById('notes');
+        const updateForm = document.getElementById('updateForm');
         const modalQuantityInput = document.getElementById('Quantity');
         // const modalImage = document.getElementById('modalImage');
-        
-        openModalButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            const detailId = this.getAttribute('data-detail-id');
-            const orderId = this.getAttribute('data-order-id');
-            const orderjumlah = this.getAttribute('data-order-jumlah');
 
-            // Update konten modal dengan data dari atribut
-            modalTitle.textContent = detailId; // Menggunakan detailId sebagai nama menu
-            modalNotes.textContent = orderId ? orderId : 'Tidak ada catatan';  // Menggunakan orderId sebagai catatan
+
+        openModalButtons.forEach(function(button) {
+            button.addEventListener('click', function(event) {
+                event.preventDefault();
+                const detailId = this.getAttribute('data-detail-id');
+                const orderId = this.getAttribute('data-order-id');
+                const order = this.getAttribute('data-der-id');
+                const detail = this.getAttribute('data-tail-id');
+                const orderjumlah = this.getAttribute('data-order-jumlah');
+
+                // Update konten modal dengan data dari atribut
+                modalTitle.textContent = detailId; 
+                modalNotes.textContent = orderId ? orderId : 'Tidak ada catatan';  
+                var action = '{{ route('kitchen.update', ['id_order' => ':id_order', 'id_order_details' => ':id_order_details']) }}';
+                action = action.replace(':id_order', order).replace(':id_order_details', detail);
+                updateForm.setAttribute('action', action);
+                modalQuantityInput.value = orderjumlah;
+            });
+        });
+
             
-            modalQuantityInput.value = orderjumlah;// Menggunakan orderId sebagai catatan
-            });
-
-        
-            });
     });
     </script>
     <script>
@@ -306,7 +291,7 @@
         // Confirm save button event listener
         const confirmSaveButton = document.getElementById('confirmSaveBtn');
         confirmSaveButton.addEventListener('click', function() {
-            alert('Changes saved!');
+            // alert('Changes saved!');
             hideModal('default-modal');
             hideModal('confirm-modal');
         });
